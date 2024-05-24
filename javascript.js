e617dbdd44990c16e0cc5a8a537e7927310ec57e
ri-ree <adrie270801@gmail.com>
2024-05-24 23:09:06 +0200

    Add document queries to submit button click event
    The form data was not getting submitted, so the document queres were moved to the button click event

--- javascript.js
+++ javascript.js
@@ -2,10 +2,7 @@ const container = document.querySelector('#books');
 const btn = document.querySelector('#btn');
 const dialog = document.querySelector('dialog');
 const closeButton = document.querySelector('dialog button');
-const formTitle = document.querySelector('#title');
-const formAuthor = document.querySelector('#author');
-const formPages = document.querySelector('#pages');
-const formRead = document.querySelector('#read');
+const submitButton = document.querySelector('#submitButton');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -20,34 +17,54 @@ function Book(title, author, pages, read) {
 
 const myLibrary = [];
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", "not read yet");
-myLibrary.push(theHobbit);
-
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 };
 
-addBookToLibrary("One Day", "David Nichols", "448", "not read yet");
+submitButton.addEventListener("click", submitClick, false);
 
-addBookToLibrary("Tomie", "Junji Ito", "752", "not read yet");
+function addedBook() {
+    for (const obj of myLibrary) {
+        let bookDisplay = document.createElement('div');
+        bookDisplay.textContent = obj.info();
+        bookDisplay.style.background = "rgb(209, 176, 187)";
+        bookDisplay.style.padding = "10px";
+        container.appendChild(bookDisplay);
+        console.log(obj);
+    };
+}
 
-addBookToLibrary("Ill Wind", "Rachel Caine", "352", "read");
+function submitClick(event) {
+    let formTitle = document.getElementById('title').value;
+    let formAuthor = document.getElementById('author').value;
+    let formPages = document.querySelector('#pages').value;
+    let formRead = document.querySelector('#read').value;
+    addBookToLibrary(formTitle, formAuthor, formPages, formRead);
+    console.log(myLibrary);
+    event.preventDefault();
+    addedBook();
+    console.log(formTitle);
+    console.log(formAuthor);
+    console.log(formPages);
+    console.log(formRead);
+};
 
-addBookToLibrary("The Lord of The Rings", "J.R.R Tolkien", "1216", "not read yet");
 
-for (const obj of myLibrary) {
+/*for (const obj of myLibrary) {
     let bookDisplay = document.createElement('div');
     bookDisplay.textContent = obj.info();
     bookDisplay.style.background = "rgb(209, 176, 187)";
     bookDisplay.style.padding = "10px";
     container.appendChild(bookDisplay);
-};
+    console.log(obj);
+};*/
 
 btn.addEventListener("click", () => {
     dialog.showModal();
 });
 
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener("click", (event) => {
+    event.preventDefault();
     dialog.close();
 });
